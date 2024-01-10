538d400e35553a2342d7a1de458af929082861a3
Niraj-Aware <92052184+Niraj-Aware@users.noreply.github.com>
2024-01-10 11:43:47 +0530

    Update web.py

--- PDD-TR/web.py
+++ PDD-TR/web.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import webbrowser
 
 def main():
     st.title("Website Redirect Buttons")
@@ -7,17 +6,17 @@ def main():
     # Create two buttons side by side
     col1, col2 = st.beta_columns(2)
 
-    if col1.button("Vegetable Plant"):
+    if col1.button("Veg Plant"):
         # Define the URL for Website 1
         website_url_1 = "https://www.example.com"
         # Open the website in a new tab
-        webbrowser.open_new_tab(website_url_1)
+        st.markdown(f"[Website 1]({website_url_1})", unsafe_allow_html=True)
 
     if col2.button("Cash Crop"):
         # Define the URL for Website 2
         website_url_2 = "https://www.example2.com"
         # Open the website in a new tab
-        webbrowser.open_new_tab(website_url_2)
+        st.markdown(f"[Website 2]({website_url_2})", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
